a9b364e52bf6343416410e15ab1e79609d7f3dfd
josezanle <63666176+josezanle@users.noreply.github.com>
2020-08-06 16:57:08 -0300

    SPA con react js,buscador de letras de canciones

---
+++ precache-manifest.bc6f419573198b9b95a9e4603a83c9c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8284e651ac129fca7797e8d9b7d91f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d75c906711cbc79b25e5",
+    "url": "/static/css/main.d8956e4b.chunk.css"
+  },
+  {
+    "revision": "3126c9dc464bd484bc44",
+    "url": "/static/js/2.d0c20912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0c20912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75c906711cbc79b25e5",
+    "url": "/static/js/main.b9e86549.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "66e2777cea01573ca363dee601cd67f0",
+    "url": "/static/media/pexels-david-bartus-435840.66e2777c.jpg"
+  }
+]);
